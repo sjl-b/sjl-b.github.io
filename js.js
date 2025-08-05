f259fd142dc82c673d159fd75f5ac3177e5d7f81
sjl-b <152614925+sjl-b@users.noreply.github.com>
2025-08-05 21:08:15 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -9,16 +9,7 @@ window.onkeydown = window.onkeyup = window.onkeypress = function (event) {
 
 
 
-//禁用调试工具
-var threshold = 160; // 打开控制台的宽或高阈值
-// 每秒检查一次
-var check = setInterval(function() {
-    if (window.outerWidth - window.innerWidth > threshold || 
-        window.outerHeight - window.innerHeight > threshold) {
-        // 如果打开控制台，则刷新页面
-        window.location.reload();
-    }
-}, 1000)
+
 
 //屏蔽右键菜单
 document.oncontextmenu = function (event){
